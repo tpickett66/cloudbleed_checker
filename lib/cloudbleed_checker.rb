180caf05371851bb2005db1db6532b92a591ded9
Tyler Pickett <t.pickett66@gmail.com>
2017-02-24 10:57:18 -0700

    add executable sekeleton and dependencies

--- lib/cloudbleed_checker.rb
+++ lib/cloudbleed_checker.rb
@@ -1,4 +1,5 @@
 require "cloudbleed_checker/version"
+require 'cloudbleed_checker/cli'
 
 module CloudbleedChecker
   # Your code goes here...
---
+++ lib/cloudbleed_checker/cli.rb
@@ -0,0 +1,9 @@
+require 'thor'
+
+module CloudbleedChecker
+  class CLI < Thor
+    desc "check", "Check all supported browsers"
+    def check
+    end
+  end
+end
